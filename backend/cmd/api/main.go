dff17ecacedce1a1bee8af7ace62197639d39589
Ashmn07 <ashwin.menon202@gmail.com>
2024-10-24 22:48:18 +0530

    Added JWT logic

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -6,7 +6,9 @@ import (
 	"github.com/Ashmn07/ticket-booking/config"
 	"github.com/Ashmn07/ticket-booking/db"
 	"github.com/Ashmn07/ticket-booking/handlers"
+	"github.com/Ashmn07/ticket-booking/middleware"
 	"github.com/Ashmn07/ticket-booking/repositories"
+	"github.com/Ashmn07/ticket-booking/services"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -21,11 +23,18 @@ func main() {
 
 	eventRepository := repositories.NewEventRepository(db)
 	ticketRepository := repositories.NewTicketRepository(db)
+	authRepository := repositories.NewAuthRepository(db)
+
+	authService := services.NewAuthService(authRepository)
 
 	server := app.Group("/api")
 
-	handlers.NewEventHandler(server.Group("/event"), eventRepository)
-	handlers.NewTicketHandler(server.Group("/ticket"), ticketRepository)
+	handlers.NewAuthHandler(server.Group("/auth"), authService)
+
+	privateRoutes := server.Use(middleware.AuthProtected(db))
+
+	handlers.NewEventHandler(privateRoutes.Group("/event"), eventRepository)
+	handlers.NewTicketHandler(privateRoutes.Group("/ticket"), ticketRepository)
 
 	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
 }
